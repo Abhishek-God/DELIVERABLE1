12ed07154047862f43de45497d59034494cfd55e
Abhishek-God <66026408+Abhishek-God@users.noreply.github.com>
2020-07-15 09:23:28 -0400

    Create Suit.java

---
+++ Blackjack/BlackjackGame/src/main/java/com/mr/abhishek/blackjackgame/Suit.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mr.abhishek.blackjackgame;
+
+/**
+ *
+ * @author mr.abhishek
+ */
+public enum Suit {
+    CLUBS, DIAMONDS, HEARTS, SPADES;
+    
+}
